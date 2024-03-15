36bf5ee56180a158fb9aba60fe77380eaf1d8866
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-15 12:04:05 -0400

    Probando hacer cambios

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void exercise_1(string s1, string s2, string s3) {
   // TODO: YOUR CODE HERE
   cout << "a" << endl;
+  cout << hello word << endl
 }
 
 void exercise_2(double A, double B, double C) {
