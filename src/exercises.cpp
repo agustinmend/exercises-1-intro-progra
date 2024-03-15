49a08efef2891a92079e0fdc6406bfd55c13d23a
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-15 12:30:50 -0400

    prueba 5 ejercicio 3

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -20,9 +20,8 @@ void exercise_3(int a, int b) {
     cout << a/b << endl;}
   else if (a=0) {
     cout << "Impossible" << endl;}
-  else (b>a) ;
+  else ;
     cout << "0" << endl;
-  
 }
 
 void exercise_4(double n, double a, double b, double x, double y) {
