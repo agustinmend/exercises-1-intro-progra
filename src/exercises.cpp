eca580cacc110c5c8d37f186eb54dccd78fa2f4b
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 01:52:49 -0400

    prueba 4 ejercicio 9

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -77,13 +77,7 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   char caracter4 = s4[0];
   char caracter5 = s5[0];
   cout << caracter1 << caracter2 << caracter3 << caracter4 << caracter5;
-  if (caracter1 == caracter5) {
-    cout << " Hemos encontrado algo!";
-  }
-  else {
-    cout << " Aun sin suerte";
-  }
-  return "";
+  
 }
 
 int exercise_10(int a, int b) {
