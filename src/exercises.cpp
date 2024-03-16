cb777476ce3646619eaa4b04d00445dbf3682ca9
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 10:55:47 -0400

    prueba ejercicio 11

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -101,10 +101,10 @@ int exercise_10(int a, int b) {
 string exercise_11(int number) {
   // TODO: YOUR CODE HERE
   if (number == 11235813) {
-    cout << "Se encontró Fibonacci" << endl;
+    return "Se encontró Fibonacci";
   }
   else {
-    cout << "Esto no es de Fibonacci" << endl;
+    return "Esto no es de Fibonacci";
   }
 }
 
