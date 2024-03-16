02cba56ddca1062be48a568bb0d9a98aaf4a71dd
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 16:21:23 -0400

    prueba ejercicio 13

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -168,14 +168,14 @@ void exercise_12(string color1, int numb1,
 
 string exercise_13(int age, int years_of_experience) {
   // TODO: YOUR CODE HERE
-  if (age >= 18 && years_of_experience < 3){
+  if (age >= 18 && years_of_experience < 3 && years_of_experience >= 0){
     return "coordinador de proyecto";
   }
   else if (age >= 18 && years_of_experience >= 3 && years_of_experience < 5){
     return "director de proyecto";
   }
   else if (age >= 18 && years_of_experience >= 5){
-    return"director senior";
+    return "director senior";
   }
   else {
     cout << "" << endl;
