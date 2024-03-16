4b6883766db700e966172b40519b088b243b7b56
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 19:04:07 -0400

    prueba ejercicio 14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -204,7 +204,7 @@ string exercise_13(int age, int years_of_experience) {
 string exercise_14(int number_of_docs) {
   // TODO: YOUR CODE HERE
   if (number_of_docs == 1 ) {
-    return "Se enontro un documento";
+    return "Se encontro un documento";
   }
   else if (number_of_docs <= 0 ) {
     return "No se encontraron documentos";
