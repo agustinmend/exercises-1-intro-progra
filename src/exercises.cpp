58d962d62f6ca2a520b4ddf651f4be2e39808616
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 16:02:00 -0400

    probar ejercicio 4

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -62,7 +62,7 @@ void exercise_4(double n, double a, double b, double x, double y) {
     cout << result << endl;
   }
   else if (n > b && n < a) {
-    result = n - n * (y/100);
+    result = b - n * (y/100);
     cout << result << endl;
   }
   else if (n > a && n > b){
