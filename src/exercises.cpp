436e9a213093fd2df55ac0801c89cafc87a7f015
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 00:51:28 -0400

    prueba 3 ejercicio 10

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -75,7 +75,7 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 }
 
 int exercise_10(int a, int b) {
-  //  TODO: YOUR CODE HERE
+  // TODO: YOUR CODE HERE
   int S =a + b;
   string S1 = to_string(S);
   int digitos = S1.size();
