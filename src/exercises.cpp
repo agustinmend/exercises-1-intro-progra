abc8103279a5cccb9171087b4ac4eb75203a8f99
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 09:19:33 -0400

    buscando code 127

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -47,14 +47,14 @@ void exercise_6(int number) {
 
 void exercise_7(double r) {
   // TODO: YOUR CODE HERE
-  /*if (r >= 0) {
-    long double result;
+  if (r >= 0) {
+    double result;
     result = 4 * 3.1416 * r * r;
     cout << result << endl;
   }
   else {
     cout << "Error: Radius cannot be negative." << endl;
-  }*/
+  }
 }
 
 void exercise_8(long int seconds) {
