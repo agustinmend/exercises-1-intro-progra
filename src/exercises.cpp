1c59f42a1a640fb8a13f1c77dfc0800dd752fcaf
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 11:45:13 -0400

    ejercicio 1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,6 +6,16 @@ using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
   // TODO: YOUR CODE HERE 
+  if (s1 < s2 && s1 < s3) {
+    cout << s1 ;
+  }
+  else if (s2 < s1 && s2 < s3) {
+    cout << s2 ;
+  }
+  else if (s3 < s1 && s3 < s1) {
+    cout << s3 ;
+  }
+
 }
 
 void exercise_2(double A, double B, double C) {
