af216b02ffcbee103d59ad71256d3cec5bfb9880
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 18:35:37 -0400

    prueba ejercicio 8

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -139,7 +139,7 @@ void exercise_8(long int seconds) {
   int hour = seconds / 3600;
   int minutes = seconds / 60;
   if (seconds >= 0){
-    cout << hour << ":" << minutes << ":" << seconds << endl;
+    cout << setw(2) << std ::setfill('0') << hour << ":" << setw(2) << std ::setfill('0') minutes << ":" << seconds << setw(2) << std ::setfill('0') <<endl;
   }
 }
 
