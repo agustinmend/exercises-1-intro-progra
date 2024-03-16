225a5ad37dc35bd9eb858046d10097813e327f63
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 18:54:17 -0400

    prueba ejercicio 14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -136,11 +136,6 @@ void exercise_7(double r) {
 
 void exercise_8(long int seconds) {
   // TODO: YOUR CODE HERE
-  long int hour = seconds / 3600;
-  long int minutes = seconds / 60;
-  if (seconds >= 0){
-    cout << getw(2) << std ::setfill('0') << hour << ":" << getw(2) << std ::setfill('0') minutes << ":" << getw(2) << std ::setfill('0') << seconds <<endl;
-  }
 }
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
