31e770cf47ebf45d4e107395514891704fd14177
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 18:08:54 -0400

    prueba ejercicio 9

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -145,14 +145,14 @@ void exercise_8(long int seconds) {
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
-  char caracter1 = s1[0];
-  char caracter2 = s2[0];
-  char caracter3 = s3[0];
-  char caracter4 = s4[0];
-  char caracter5 = s5[0];
+  char caracter1 = s1 [0];
+  char caracter2 = s2 [0];
+  char caracter3 = s3 [0];
+  char caracter4 = s4 [0];
+  char caracter5 = s5 [0];
   cout << s1 << s2 << s3 << s4 << s5 ;
   if ( s1 == s2) {
-    return "Hemos encontrado algo!";
+    return"Hemos encontrado algo!";
   }
   else {
     return "Aun sin suerte";
