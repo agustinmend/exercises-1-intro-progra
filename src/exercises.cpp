a686f566d922005f20d228785bd78ead6993eb13
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 11:27:44 -0400

    prueba ejercicio 5

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
-  // TODO: YOUR CODE HERE
+  // TODO: YOUR CODE HERE 
 }
 
 void exercise_2(double A, double B, double C) {
