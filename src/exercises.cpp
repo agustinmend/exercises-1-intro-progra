90ea635949da85b9f11d22271752a7ab4f3fbfec
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-16 21:49:05 -0400

    prueba ejercicio 8

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -139,7 +139,7 @@ void exercise_8(long int seconds) {
   int hour = seconds / 3600;
   int minutes = seconds % 3600;
   int result_second = minutes % 60;
-    if (seconds < 0);{
+  if (seconds < 0);{
     cout << "Error: Input seconds cannot be negative." << endl;
     }
     else if (seconds >= 0 && hour < 10 && minutes < 10 && result_seconds < 10) {
