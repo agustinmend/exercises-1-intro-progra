330efac52fd8ddc2b2c1f6223b464775ca63399f
agustinmend <160260035+agustinmend@users.noreply.github.com>
2024-03-25 17:40:23 -0400

    prueb ejercicio 15

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -251,7 +251,7 @@ void exercise_15(int a, int b, int c) {
   int a1 = c;
   int b1 = a;
   int c1 = b;
-  cout << "Los valores son: a = " << a << " b = " << b << " c = " << c << endl;
+  cout << "Los valores son: a = " << a << " b = " << b << " y c = " << c << endl;
   cout << "Permutamos: a => b, b => c, c => a" << endl;
   cout << "Los valores despues de la permutacion son: a = " << a1 << " b = " << b1 << " c = " << c1 << endl;
 }
